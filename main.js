f6377d97368fa50b1f2d20b35d1670d16577a627
Mark McLachlan <S3778046@student.rmit.edu.au>
2023-02-26 23:59:54 +1100

    Changes to all features

--- main.js
+++ main.js
@@ -1,3 +1,58 @@
+// *************************************************************************************************
+//                              GSAP ScrollTrigger
+// *************************************************************************************************
+
+gsap.from("#home", {
+    duration: 1,
+    x: "-1500",
+    scrollTrigger: "#home"
+  });
+
+gsap.from("#cheetah", {
+    duration: 1,
+    x: "-1500",
+    scrollTrigger: "#cheetah"
+  });
+
+  gsap.from("#conservation", {
+    duration: 1,
+    x: "-1500",
+    scrollTrigger: "#conservation"
+  });
+
+  gsap.from("#about", {
+    duration: 1,
+    x: "-1500",
+    scrollTrigger:"#about"
+  });
+
+  gsap.from("#contact", {
+    duration: 1,
+    x: "-1500",
+    scrollTrigger: "#contact",
+  });
+
+
+  // *************************************************************************************************
+//                              GSAP 
+// *************************************************************************************************
+const tl = new TimelineLite();
+tl.from('header', 2, {
+        y: '-400',
+        ease: "bounce",  
+})
+
+function animateSection(section) {
+
+    const tl = new TimelineLite();
+    tl.from(section, 1, {
+        x: "1700",
+    })
+};
+
+
+
+
 // ************************************************************************************
 //                      Dropdown menu functionality 
 // *************************************************************************************
@@ -18,7 +73,6 @@ openDropdownMenu.addEventListener('click', function() {
 function closeMenu() {
     document.querySelector("#dropdown-menu").style.display = "none";
 }
-
 
 // ***************************************************************************************
 //    Open sections and change active link on cheetah and conservation menu. 
@@ -47,8 +101,6 @@ function openSection(sections, newSection, Links, newLink) {
 
     // Change newSection to display block
     changeToSection.style.display = "flex";
-
-    animateHeadings()
 }
 
 // ***************************************************************************************
@@ -83,8 +135,6 @@ function openSectionMobile(sections, newSection, Links, newLink, label) {
    // Change mobile menu label
    mobileLabelCheetah.innerHTML = label;
    mobileLabelConservation.innerHTML = label;
-
-   animateHeadings()
 }
 
 
@@ -110,29 +160,115 @@ window.addEventListener('scroll', () => {
 });
 
 
+// *************************************************************************************************
+//                              Region tooltips
+// *************************************************************************************************
+function displayHotSpotData(region) {
 
-function animateHeadings() {
-    const tl = new TimelineLite();
-
-    tl.from('.headings', .5, {
-        x: '-900',
-        y: '-300'
+    const countries = document.querySelectorAll('.region-tooltip-content');
+    countries.forEach(country => {
+        country.style.display = 'none';
     })
+
+    const displayRegion = document.querySelector(region);
+    displayRegion.style.display = 'block';
 }
 
+// *************************************************************************************************
+//                                  Population timeline
+// *************************************************************************************************
+function togglePopulation(tipToActive, population){
 
-// const tl = new TimelineLite();
+    // Declare variable
+    let tips = document.querySelectorAll(".tip");
+    let populationNumber = document.querySelector(".population-number");
 
-// tl.from('.part-two', 1, {
-//     y: '-900',
-// })
-// .from('.front', 0.5, {
-//     rotationY: "60deg"
-// }, "-=0.2")
-// .from('.home-item h1', 0.5, {
-//         x: '-900',
-// })
-// .from('.play', 0.5, {
-//     y: '-900',
-// })
+    //  Loop through tips and remove active class from all
+    for(let i = 0; i < tips.length; i++) {
+      tips[i].className = tips[i].className.replace(" population-active", "");
+    }
+
+    //  Add active class to current tip.
+    tips[tipToActive].className += " population-active";
+
+    // update population figure.
+    populationNumber.innerHTML = population;
+
+    const tl = new TimelineLite();
+    tl.from('.population-number', 1, {
+        x: '-500',
+            rotate: '360',
+            // ease: "bounce",  
+    })
+
+}
+
+// *************************************************************************************************
+//                              Select image
+// *************************************************************************************************
+const images = document.querySelectorAll('.slider-img');
+const imageWrapper = document.querySelector('.gallery-image');
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        const img = document.querySelector('#main-img');
+        img.src = image.src;
+        imageWrapper.style.display = 'flex';
+    })
+})
+
+// *************************************************************************************************
+//                              Image Slider
+// *************************************************************************************************
+const selectorLeft = document.querySelector('#left-arrow');
+const selectorRight = document.querySelector('#right-arrow');
+const allImages = document.querySelectorAll('.slider-img');
+
+// Functionality for image carousel right click button.
+selectorRight.addEventListener('click', function() {
+
+    // Store last image in variable
+    const lastImageSrc = allImages[8].src
+
+    // Loop through all iamges and change src to the next img src.
+    for(let i = allImages.length; i > 0; i--) {
+
+        if(i < 9) {
+            allImages[i].src = allImages[i - 1].src;
+        }   
+    }
+
+    // Change first img src to the src of the last img.
+    allImages[0].src = lastImageSrc;
+})
+
+// Functionality for image carousel Left click button.
+selectorLeft.addEventListener('click', function() {
+
+    // Store last image in variable
+    const firstImageSrc = allImages[0].src
+
+    // Loop through all iamges and change src to the next img src.
+    for(let i = 0; i < allImages.length - 1; i++) {
+
+            allImages[i].src = allImages[i + 1].src;
+    }
+
+    // Change last img src to the src of the first img.
+    allImages[8].src = firstImageSrc;
+})
+
+function openVideo() {
+    const video = document.querySelector('.video-wrapper');
+    video.style.display = 'flex';
+}
+
+function closeVideo() {
+    const video = document.querySelector('.video-wrapper');
+    video.style.display = 'none';
+}
+
+function closeImage() {
+    const img = document.querySelector('.gallery-image');
+    img.style.display = 'none';
+}
 
